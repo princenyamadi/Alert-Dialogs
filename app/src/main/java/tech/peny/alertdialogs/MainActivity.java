9e795b9e3e68bf20c371989bf83645cb86630cde
princenyamadi <princenyamadi.pn@gmail.com>
2020-12-04 15:40:20 +0000

    [feat]: Added dialog alert

--- app/src/main/java/tech/peny/alertdialogs/MainActivity.java
+++ app/src/main/java/tech/peny/alertdialogs/MainActivity.java
@@ -1,8 +1,11 @@
 package tech.peny.alertdialogs;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new AlertDialog.Builder(this)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setTitle("Are you sure!?")
+                .setMessage("Do you definitely want to do this?")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this, "it's done!",Toast.LENGTH_SHORT).show();
+
+                    }
+                })
+                .setNegativeButton("No", null)
+                .show();
     }
 }
